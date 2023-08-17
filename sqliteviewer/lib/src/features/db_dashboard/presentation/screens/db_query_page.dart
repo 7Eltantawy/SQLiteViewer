667c731fa60a705aee3beefb0f7ef9a87370c9bf
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-18 02:56:06 +0300

    Update db_query_page.dart

--- sqliteviewer/lib/src/features/db_dashboard/presentation/screens/db_query_page.dart
+++ sqliteviewer/lib/src/features/db_dashboard/presentation/screens/db_query_page.dart
@@ -1,96 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sqliteviewer/src/features/db_dashboard/presentation/components/formatter/sql_keywords_formatter.dart';
 import 'package:sqliteviewer/src/features/db_dashboard/presentation/components/formatter/test.dart';
-import 'package:sqliteviewer/src/core/helpers/db_helper.dart';
 import 'package:sqliteviewer/src/core/sql/keywords.dart';
+import 'package:sqliteviewer/src/features/db_dashboard/presentation/controller/cubit/db_dashboard_cubit.dart';
 import 'package:sqliteviewer/src/features/db_table_viewer/presentation/components/table_content_data_grid.dart';
 import 'package:sqliteviewer/src/core/widgets/loading.dart';
 
-class DBQueryPage extends StatefulWidget {
+class DBQueryPage extends StatelessWidget {
   const DBQueryPage({super.key});
 
   @override
-  State<DBQueryPage> createState() => _DBQueryPageState();
-}
-
-class _DBQueryPageState extends State<DBQueryPage> {
-  final TextEditingController sqlCodeController = TextEditingController();
-  List<Map<String, dynamic>> result = [];
-  bool isQuerying = false;
-
-  Future query() async {
-    setState(() {
-      isQuerying = true;
-    });
-
-    try {
-      result =
-          await DatabaseHelper.instance.query(sqlCodeController.text.trim());
-    } catch (e) {
-      result = [
-        {"SQLite Viewer Error": e.toString()}
-      ];
-    }
-
-    setState(() {
-      isQuerying = false;
-    });
-  }
-
-  @override
-  void dispose() {
-    sqlCodeController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Card(
-            child: Row(
-              children: [
-                isQuerying
-                    ? const Loading()
-                    : IconButton(
-                        icon: const Icon(
-                          Icons.play_circle_outline,
-                        ),
-                        onPressed: () async {
-                          await query();
-                        },
-                      )
-              ],
-            ),
+    return BlocBuilder<DbDashboardCubit, DbDashboardState>(
+      builder: (context, state) {
+        return Scaffold(
+          body: Column(
+            children: [
+              Card(
+                child: Row(
+                  children: [
+                    state.isQuerying
+                        ? const Loading()
+                        : IconButton(
+                            icon: const Icon(
+                              Icons.play_circle_outline,
+                            ),
+                            onPressed: () async {
+                              context.read<DbDashboardCubit>().query();
+                            },
+                          )
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: Scrollbar(
+                thumbVisibility: true,
+                child: TextField(
+                  controller:
+                      context.read<DbDashboardCubit>().sqlCodeController,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  expands: true,
+                  decoration: const InputDecoration(),
+                  inputFormatters: [
+                    ColoredTextFormatter(sqliteReservedKeywords),
+                  ],
+                ),
+              )),
+              SQLCodePreview(
+                text: context.read<DbDashboardCubit>().sqlCodeController.text,
+                keywords: sqliteReservedKeywords,
+              ),
+              Expanded(
+                  child: Card(
+                child: TableContentDataGrid(data: state.result),
+              )),
+            ],
           ),
-          Expanded(
-              child: Scrollbar(
-            thumbVisibility: true,
-            child: TextField(
-              controller: sqlCodeController,
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
-              expands: true,
-              decoration: const InputDecoration(),
-              inputFormatters: [
-                ColoredTextFormatter(sqliteReservedKeywords),
-              ],
-              onChanged: (value) {
-                setState(() {});
-              },
-            ),
-          )),
-          SQLCodePreview(
-            text: sqlCodeController.text,
-            keywords: sqliteReservedKeywords,
-          ),
-          Expanded(
-              child: Card(
-            child: TableContentDataGrid(data: result),
-          )),
-        ],
-      ),
+        );
+      },
     );
   }
 }
