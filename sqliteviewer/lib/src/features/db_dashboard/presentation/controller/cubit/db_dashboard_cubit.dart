82f42ae4f320461977d899b7fd1e49135cae2e7d
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-18 02:53:17 +0300

    DbDashboardState

--- sqliteviewer/lib/src/features/db_dashboard/presentation/controller/cubit/db_dashboard_cubit.dart
+++ sqliteviewer/lib/src/features/db_dashboard/presentation/controller/cubit/db_dashboard_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:sqliteviewer/src/core/helpers/db_helper.dart';
 import 'package:sqliteviewer/src/core/utils/print.dart';
 
@@ -7,8 +8,14 @@ part 'db_dashboard_state.dart';
 
 class DbDashboardCubit extends Cubit<DbDashboardState> {
   final String dbPath;
+  final TextEditingController sqlCodeController = TextEditingController();
   DbDashboardCubit(this.dbPath)
-      : super(const DbDashboardState(isLoading: true, tables: [])) {
+      : super(const DbDashboardState(
+          isLoading: true,
+          tables: [],
+          result: [],
+          isQuerying: false,
+        )) {
     DatabaseHelper.path = dbPath;
     loadTables();
   }
@@ -19,8 +26,28 @@ class DbDashboardCubit extends Cubit<DbDashboardState> {
     emit(state.copyWith(tables: tables, isLoading: false));
   }
 
+  Future query() async {
+    emit(state.copyWith(isQuerying: true));
+
+    late final List<Map<String, dynamic>> result;
+    try {
+      result =
+          await DatabaseHelper.instance.query(sqlCodeController.text.trim());
+    } catch (e) {
+      result = [
+        {"SQLite Viewer Error": e.toString()}
+      ];
+    }
+
+    emit(state.copyWith(
+      isQuerying: false,
+      result: result,
+    ));
+  }
+
   @override
   Future<void> close() {
+    sqlCodeController.dispose();
     try {
       DatabaseHelper.instance.close();
     } catch (e) {
--- sqliteviewer/lib/src/features/db_dashboard/presentation/controller/cubit/db_dashboard_state.dart
+++ sqliteviewer/lib/src/features/db_dashboard/presentation/controller/cubit/db_dashboard_state.dart
@@ -1,24 +1,32 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'db_dashboard_cubit.dart';
 
 class DbDashboardState extends Equatable {
   final bool isLoading;
+  final bool isQuerying;
   final List<String> tables;
+  final List<Map<String, dynamic>> result;
+
   const DbDashboardState({
     required this.isLoading,
+    required this.isQuerying,
     required this.tables,
+    required this.result,
   });
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [isLoading, isQuerying, tables, result];
 
   DbDashboardState copyWith({
     bool? isLoading,
+    bool? isQuerying,
     List<String>? tables,
+    List<Map<String, dynamic>>? result,
   }) {
     return DbDashboardState(
       isLoading: isLoading ?? this.isLoading,
+      isQuerying: isQuerying ?? this.isQuerying,
       tables: tables ?? this.tables,
+      result: result ?? this.result,
     );
   }
 }
