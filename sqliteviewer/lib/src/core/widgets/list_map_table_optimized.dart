bfcd57354e302b561dd66f0bdd344675d44cd3e3
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-17 21:38:21 +0300

    Update list_map_table_optimized.dart

--- sqliteviewer/lib/src/core/widgets/list_map_table_optimized.dart
+++ sqliteviewer/lib/src/core/widgets/list_map_table_optimized.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqliteviewer/src/core/utils/print.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class ListMapTablePagination extends StatefulWidget {
@@ -19,35 +20,79 @@ class _ListMapTablePaginationState extends State<ListMapTablePagination> {
 
   @override
   void initState() {
-    dataSource = _ListMapDataSource(
-      widget.data,
-      _rowsPerPage,
-      (value) {},
-    );
+    getData();
     super.initState();
   }
 
-  Widget _buildDataGrid() {
-    return SfDataGrid(
-      source: dataSource,
-      rowsPerPage: _rowsPerPage,
-      gridLinesVisibility: GridLinesVisibility.both,
-      headerGridLinesVisibility: GridLinesVisibility.both,
-      allowColumnsResizing: true,
-      allowEditing: true,
-      allowSorting: true,
-      allowFiltering: true,
-      columnWidthMode: ColumnWidthMode.fitByColumnName,
-      highlightRowOnHover: true,
-      columns: dataSource.header(),
+  getData() {
+    dataSource = _ListMapDataSource(
+      widget.data,
+      _rowsPerPage,
+      (value) => showCellContent(value),
     );
+  }
+
+  @override
+  void didUpdateWidget(covariant ListMapTablePagination oldWidget) {
+    if (oldWidget.data != widget.data) {
+      getData();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void showCellContent(String value) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            // constraints: const BoxConstraints(minHeight: 100, maxHeight: 350),
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Scrollbar(
+                child: ListView(
+                  padding: const EdgeInsets.all(10.0),
+                  shrinkWrap: true,
+                  children: [
+                    SelectableText(
+                      value.toString(),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildDataGrid() {
+    return widget.data.isEmpty
+        ? const SizedBox()
+        : SfDataGrid(
+            source: dataSource,
+            rowsPerPage: _rowsPerPage,
+            gridLinesVisibility: GridLinesVisibility.both,
+            headerGridLinesVisibility: GridLinesVisibility.both,
+            allowColumnsResizing: true,
+            allowEditing: true,
+            allowSorting: true,
+            allowFiltering: true,
+            columnWidthMode: ColumnWidthMode.fitByColumnName,
+            highlightRowOnHover: true,
+            columns: dataSource.header(),
+          );
   }
 
   Widget _bottomBar() {
     return SfDataPager(
       delegate: dataSource,
       availableRowsPerPage: const <int>[15, 25, 50, 100, 250, 500],
-      pageCount: (widget.data.length / _rowsPerPage).ceil().toDouble(),
+      pageCount: widget.data.isEmpty
+          ? 1
+          : (widget.data.length / _rowsPerPage).ceil().toDouble(),
       onRowsPerPageChanged: (int? rowsPerPage) {
         setState(() {
           _rowsPerPage = rowsPerPage!;
@@ -58,10 +103,6 @@ class _ListMapTablePaginationState extends State<ListMapTablePagination> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget.data.isEmpty) {
-      return const SizedBox();
-    }
-
     return Scaffold(
       body: _buildDataGrid(),
       bottomNavigationBar: _bottomBar(),
@@ -77,23 +118,27 @@ class _ListMapDataSource extends DataGridSource {
   final ValueChanged<String> onCellPressed;
 
   /// Table Header
-  List<GridColumn> header() => <GridColumn>[
-        for (final key in _data.first.keys)
-          GridColumn(
-            columnName: key,
-            label: Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.centerRight,
-              child: Text(
-                key,
-                style: const TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
+  List<GridColumn> header() {
+    appPrint(_data);
+    appPrint(_data.isEmpty);
+    return <GridColumn>[
+      for (final key in _data.first.keys)
+        GridColumn(
+          columnName: key,
+          label: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.centerRight,
+            child: Text(
+              key,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-      ];
+        ),
+    ];
+  }
 
   @override
   List<DataGridRow> get rows {
@@ -112,24 +157,22 @@ class _ListMapDataSource extends DataGridSource {
   }
 
   DataGridCell buildDataCell(MapEntry<String, dynamic> entry) {
-    num? numValue = num.tryParse(entry.value.toString());
-    if (numValue != null) {
-      if (numValue is int) {
+    switch (entry.value.runtimeType) {
+      case int:
         return DataGridCell<int>(
           columnName: entry.key,
           value: entry.value,
         );
-      } else {
+      case double:
         return DataGridCell<double>(
           columnName: entry.key,
           value: entry.value,
         );
-      }
-    } else {
-      return DataGridCell<String>(
-        columnName: entry.key,
-        value: entry.value.toString(),
-      );
+      default:
+        return DataGridCell<String>(
+          columnName: entry.key,
+          value: entry.value.toString(),
+        );
     }
   }
 
@@ -142,9 +185,16 @@ class _ListMapDataSource extends DataGridSource {
             onTap: () {
               onCellPressed.call(e.value.toString());
             },
-            child: Container(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(e.value.toString()),
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  e.value.toString(),
+                  style: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
             ),
           );
         },
