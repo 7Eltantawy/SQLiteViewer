8edc4609071307f9b44e2162ae6231bcb6130158
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-19 18:55:56 +0300

    Update sql_code_preview.dart

--- sqliteviewer/lib/src/features/db_dashboard/presentation/components/formatter/sql_code_preview.dart
+++ sqliteviewer/lib/src/features/db_dashboard/presentation/components/formatter/sql_code_preview.dart
@@ -15,7 +15,7 @@ class SQLCodePreview extends StatelessWidget {
     required this.dataTypeKeywords,
     required this.text,
     required this.tablesColumns,
-    this.colorSettings = SQLCodePreviewColorSettings.colorScheme2,
+    this.colorSettings = SQLCodePreviewColorSettings.colorScheme,
   }) : tableColumnsNames = tablesColumns.entries.fold(
           <String>[],
           (previousValue, element) => previousValue
@@ -206,74 +206,7 @@ class SQLCodePreviewColorSettings {
     required this.specialCharsColor,
   });
 
-  // High Contrast Theme 1
-  static const highContrast1 = SQLCodePreviewColorSettings(
-    keywordsColor: Colors.white,
-    dataTypeKeywords: Colors.yellow,
-    tablesColor: Colors.orange,
-    tablesColumnsColor: Colors.blue,
-    commentColor: Colors.green,
-    quotedColor: Colors.purple,
-    specialCharsColor: Colors.red,
-  );
-
-  // High Contrast Theme 2
-  static const highContrast2 = SQLCodePreviewColorSettings(
-    keywordsColor: Colors.green,
-    dataTypeKeywords: Colors.purple,
-    tablesColor: Colors.yellow,
-    tablesColumnsColor: Colors.teal,
-    commentColor: Colors.amber,
-    quotedColor: Colors.blue,
-    specialCharsColor: Colors.cyan,
-  );
-
-  // High Contrast Theme 3
-  static const highContrast3 = SQLCodePreviewColorSettings(
-    keywordsColor: Colors.blue,
-    dataTypeKeywords: Colors.orange,
-    tablesColor: Colors.green,
-    tablesColumnsColor: Colors.red,
-    commentColor: Colors.pink,
-    quotedColor: Colors.teal,
-    specialCharsColor: Colors.amber,
-  );
-
-  // High Contrast Theme 4
-  static const highContrast4 = SQLCodePreviewColorSettings(
-    keywordsColor: Colors.pink,
-    dataTypeKeywords: Colors.blue,
-    tablesColor: Colors.cyan,
-    tablesColumnsColor: Colors.purple,
-    commentColor: Colors.yellow,
-    quotedColor: Colors.green,
-    specialCharsColor: Colors.red,
-  );
-
-  // High Contrast Theme 5
-  static const highContrast5 = SQLCodePreviewColorSettings(
-    keywordsColor: Colors.yellow,
-    dataTypeKeywords: Colors.green,
-    tablesColor: Colors.purple,
-    tablesColumnsColor: Colors.orange,
-    commentColor: Colors.red,
-    quotedColor: Colors.cyan,
-    specialCharsColor: Colors.blue,
-  );
-
-  // Color Scheme 1
-  static const colorScheme1 = SQLCodePreviewColorSettings(
-    keywordsColor: Color(0xFF1976D2),
-    dataTypeKeywords: Color(0xFF4CAF50),
-    tablesColor: Color(0xFFF44336),
-    tablesColumnsColor: Color(0xFF673AB7),
-    commentColor: Color(0xFF9E9E9E),
-    quotedColor: Color(0xFFFF9800),
-    specialCharsColor: Color(0xFF607D8B),
-  );
-
-  // Color Scheme 2
-  static const colorScheme2 = SQLCodePreviewColorSettings(
+  static const colorScheme = SQLCodePreviewColorSettings(
     keywordsColor: Color(0xFFE91E63),
     dataTypeKeywords: Color(0xFF2196F3),
     tablesColor: Color(0xFF8BC34A),
@@ -281,38 +214,5 @@ class SQLCodePreviewColorSettings {
     commentColor: Color(0xFF795548),
     quotedColor: Color(0xFF9C27B0),
     specialCharsColor: Color(0xFFCDDC39),
-  );
-
-  // Color Scheme 3
-  static const colorScheme3 = SQLCodePreviewColorSettings(
-    keywordsColor: Color(0xFF009688),
-    dataTypeKeywords: Color(0xFFFFC107),
-    tablesColor: Color(0xFF03A9F4),
-    tablesColumnsColor: Color(0xFF8BC34A),
-    commentColor: Color(0xFF9E9E9E),
-    quotedColor: Color(0xFF673AB7),
-    specialCharsColor: Color(0xFF607D8B),
-  );
-
-  // Color Scheme 4
-  static const colorScheme4 = SQLCodePreviewColorSettings(
-    keywordsColor: Color(0xFF673AB7),
-    dataTypeKeywords: Color(0xFF4CAF50),
-    tablesColor: Color(0xFF2196F3),
-    tablesColumnsColor: Color(0xFFE91E63),
-    commentColor: Color(0xFF9E9E9E),
-    quotedColor: Color(0xFFFFC107),
-    specialCharsColor: Color(0xFF795548),
-  );
-
-  // Color Scheme 10
-  static const colorScheme5 = SQLCodePreviewColorSettings(
-    keywordsColor: Color(0xFFE57373),
-    dataTypeKeywords: Color(0xFF81C784),
-    tablesColor: Color(0xFF64B5F6),
-    tablesColumnsColor: Color(0xFFF06292),
-    commentColor: Color(0xFFB0BEC5),
-    quotedColor: Color(0xFFFFD54F),
-    specialCharsColor: Color(0xFFA1887F),
   );
 }
