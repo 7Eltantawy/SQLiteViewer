33db43d19804707fcaa44290bb4508560cea98b8
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-20 03:52:36 +0300

    Windows Support

--- sqliteviewer/lib/src/services.dart
+++ sqliteviewer/lib/src/services.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future<void> initServices() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  if (Platform.isWindows) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 }
