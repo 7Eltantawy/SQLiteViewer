4bbef5c2a30658549d50503f8737280ce91e0f0f
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-24 20:17:07 +0300

    File name using basename

--- sqliteviewer/lib/src/core/extension/file.dart
+++ sqliteviewer/lib/src/core/extension/file.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
+import "package:path/path.dart";
 
 extension FileExt on File {
   /// Get FIle name from its path
   String getFileName() {
-    return path.split("/").last;
+    return basename(path);
   }
 }
--- sqliteviewer/lib/src/core/extension/string.dart
+++ sqliteviewer/lib/src/core/extension/string.dart
@@ -1,6 +1,8 @@
+import 'package:path/path.dart';
+
 extension StringExt on String {
   /// Get FIle name from its path
   String getFileName() {
-    return split("/").last;
+    return basename(this);
   }
 }
